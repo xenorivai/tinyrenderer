504075ed9d3933badf2fbdbf4350a53ba6092c55
Samyak Wanjarwadkar <samyak_kw@ma.iitr.ac.in>
2021-10-23 21:19:19 +0530

    Added modelview and improved projection matrices

--- main.cpp
+++ main.cpp
@@ -15,13 +15,15 @@ const int height = 1200;
 const int depth = 255;
 
 
-vec3f cam(0,0,3);
+vec3f cam(0.8, 0.7, 5.0);
+vec3f target(9.0,0.15,1.5);
+
+// vec3f cam(0,0,9);
+// vec3f target(1,1,3);
 
 
 Model* model = NULL;
 double *zbuffer = NULL;
-
-
 
 mat4 viewport(int x_v , int y_v , int w , int h){
 /*
@@ -78,6 +80,68 @@ mat4 viewport(int x_v , int y_v , int w , int h){
 
 }
 
+mat4 lookat(vec3f camPos, vec3f targetPos,vec3f globalUp = vec3f(0.0,1.0,0.0)){
+/*
+	Imitates glm::lookAt()
+	Returns a matrix that transforms local space to view/camera space
+	Need to transform local space coords to world space coords then apply camera transform to transform all vertices as if viewed from the camera
+	Inputs needed would be :
+		1. camera pos
+		2. target pos
+		3. global_up vector(global up vector)
+	
+	Aim is to perform a change of basis, that is to change from standard 3d basis to a basis relative to camera pos,
+	The basis vectors would be : cameraDir(from camPos to targetPos) , camUp , camRight
+
+	camDir = (camPos - targetPos).normalize
+	camRight = cross(global_up,camDir).normalize
+	camUp = cross(camDir,camRight).normalize
+
+	There now we have the new basis vectors, just calculate a change of basis matrix to oreint everything according to the camera , also translate everything opposite to camera's position since after change of basis camera must be at the origin looking down the negative z axis
+
+
+	This gives us the matrices: 
+	   	(1)The change of basis matrix is : 
+			
+				[		|		  |		    |		]
+				[	camRight	camUp	 camDir		]
+				[		|		  |			|		]
+				[		|		  |			|		]
+
+		Now is actuality we are not moving around with our camera but ratheer moving the scene opp to motion of camera , hence for a true transform , we need to multiply with inverse of the above matrix , and it happend to be that the above matrix is orthogonal hence , its inverse is its TRANSPOSE
+
+		(2)Translation Matrix , again since motion is opp to camera , we translate everything by (-camPos)
+				The matrix is : 
+					[1		0		0		-camPos.x	]
+					[0		1		0		-camPos.y	]
+					[0		0		1		-camPos.z	]
+					[0		0		0			1		]
+
+			If we apply these two matrices we get what is called the Model View matrix/transform we simply transforms all coords as if being viewed from the camera.
+*/
+
+	mat4 m = mat4::identity();
+	vec3f camDir = (camPos - targetPos).normalize();
+	// vec3f camDir = (targetPos - camPos).normalize();
+	vec3f camRight = cross(globalUp,camDir).normalize();
+	vec3f camUp = cross(camDir,camRight).normalize();
+
+
+	m[0][0] = camRight.x;	m[0][1] = camRight.y;	m[0][2] = camRight.z;	m[0][3] = -camPos.x;
+	m[1][0] = camUp.x;		m[1][1] = camUp.y;		m[1][2] = camUp.z;		m[1][3] = -camPos.y;
+	m[2][0] = camDir.x;		m[2][1] = camDir.y;		m[2][2] = camDir.z;		m[2][3] = -camPos.z;
+	m[3][0] = 0;			m[3][1] = 0;			m[3][2] = 0;			m[3][3] = 1;
+
+	return m;
+}
+
+mat4 projection()
+{
+    mat4 matrix = mat4::identity();
+    matrix[3][2] = -1.f / (cam - target).norm();
+    return matrix;
+}
+
 void INIT_ZBUF(void){
 	zbuffer = new double[width*height];
 	for(int i = width*height ; i >= 0 ; i--){
@@ -128,11 +192,11 @@ void untex_render(vec3f light_dir, double *zbuffer, Model *model, TGAImage &imag
 
 
 void render(vec3f light_dir, double *zbuffer, Model *model, TGAImage &image) {
-
-	mat4 view_port = viewport(width/8,height/8,width*3/4,height*3/4);
-	// mat4 view_port = viewport(0,0,width,height);
-	mat4 proj = mat4::identity();
-	proj[3][2] = 1.0f/cam.z;
+	
+	mat4 view_port = viewport(0,0,width,height);
+	// mat4 view_port = viewport(width/8,height/8,width*3/4,height*3/4);
+	mat4 proj = projection();
+	mat4 model_view = lookat(cam,target,vec3f(0,1,0));
 
 	for (size_t i = 0; i < model->nfaces(); i++) {
 
@@ -145,7 +209,7 @@ void render(vec3f light_dir, double *zbuffer, Model *model, TGAImage &image) {
 			vec3f v = model->vert(i, j); //get the jth vertex of ith face, these have components as just numbers in the range [-1,1]
 
 			world_coords[j] = v;
-			screen_coords[j] = embed<3>(view_port*proj*embed<4>(v));
+			screen_coords[j] = embed<3>(view_port*proj*model_view*embed<4>(v));
 			uv_coords[j] = model->uv(i,j);			
 		}
 
@@ -161,13 +225,13 @@ void render(vec3f light_dir, double *zbuffer, Model *model, TGAImage &image) {
 		}
 	}
 
-	// TGAImage depth(width, height, TGAImage::GRAYSCALE);
-	// for(size_t h = 0 ; h < height ; h++){
-	// 	for(size_t w = 0 ; w < width ; w++){
-	// 		depth.set(h,w,static_cast<uint8_t>((zbuffer[w * height + h] + 1) * 128));
-	// 	}
-	// }
-	// depth.write_tga_file("depth.tga");
+	TGAImage depth(width, height, TGAImage::GRAYSCALE);
+	for(size_t h = 0 ; h < height ; h++){
+		for(size_t w = 0 ; w < width ; w++){
+			depth.set(h,w,static_cast<uint8_t>((zbuffer[w * height + h] + 1) * 255));
+		}
+	}
+	depth.write_tga_file("depth.tga");
 }
 
 
@@ -185,7 +249,8 @@ int main(int argc, char** argv) {
 
 	TGAImage image(width, height, TGAImage::RGB);
 
-	vec3f light_dir (0,0,-1);
+	vec3f light_dir = (target-cam).normalize();
+	// vec3f light_dir = vec3f(1,-1,1).normalize();
 
 	// untex_render(light_dir,zbuffer,model,image);
 	render(light_dir,zbuffer,model,image);
